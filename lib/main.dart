98411a4133908f0ae19df85d9080642a6c6b8947
KaylaSyahma <ksyahma22@gmail.com>
2025-01-20 09:41:26 +0700

    konfigurasi firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,36 @@
+import 'package:attendance_app/ui/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(AttendanceApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      // data diambil dri file google.services.json
+      apiKey:  "AIzaSyDw-1OklhMbSqTy25KjR7x_QZpE8wkJybM", //current key
+      appId: "1:250012638481:android:4a66e31241bfc0fae680b0", //mobile_sdk_app dri json
+      messagingSenderId: "250012638481", //project number dri json
+      projectId: "attendance-app-b1376" //project_id dri json
+    )
+  );
+  runApp(const AttendanceApp());
 }
 
-class AttendanceApp extends StatefulWidget {
+// initializeapp harus dipanggil sebelum kita memanggil layanan firebase lainnya
+
+class AttendanceApp extends StatelessWidget {
   const AttendanceApp({super.key});
 
   @override
-  State<AttendanceApp> createState() => _AttendanceAppState();
-}
-
-class _AttendanceAppState extends State<AttendanceApp> {
-  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      theme: ThemeData(
+        cardTheme: CardTheme(surfaceTintColor: Colors.white,),
+        dialogTheme: DialogTheme(surfaceTintColor: Colors.white, backgroundColor: Colors.white,),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        useMaterial3: true
+      ),
+      home: const HomeScreen(),
+    );
   }
 }
