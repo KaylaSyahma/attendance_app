7adf2d553d56db86791d3bc68f46108b5ed7e204
KaylaSyahma <ksyahma22@gmail.com>
2025-01-21 09:07:50 +0700

    initial config for face detection feature

--- lib/utility/vision.dart
+++ lib/utility/vision.dart
@@ -1,6 +1,12 @@
+import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
+
 class Vision {
   // dibuat privat karena akan mendeteksi wajah
   Vision._();
-
+  // ini adalah syntax untuk kelas kita menjadi safe
   static final Vision instance = Vision._();
+
+  FaceDetector faceDetector([FaceDetectorOptions? options]) {
+    return FaceDetector(options: options?? FaceDetectorOptions());
+  }
 }
