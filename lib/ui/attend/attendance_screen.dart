4a92f707cf7bb7fe8551c9323ae2d461c65b3381
KaylaSyahma <ksyahma22@gmail.com>
2025-01-22 10:27:00 +0700

    creating camerabox

--- lib/ui/attend/attendance_screen.dart
+++ lib/ui/attend/attendance_screen.dart
@@ -1,10 +1,142 @@
+import 'dart:io';
+
+import 'package:attendance_app/ui/attend/camera_screen.dart';
+import 'package:camera/camera.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
-class Attendance  extends StatelessWidget {
-  const Attendance ({super.key});
+class AttendanceScreen extends StatefulWidget {
+  final XFile? image;
+
+  const AttendanceScreen({super.key, required this.image});
 
   @override
+  State<AttendanceScreen> createState() => _AttendanceScreenState(this.image);
+}
+
+class _AttendanceScreenState extends State<AttendanceScreen> {
+  _AttendanceScreenState(this.image);
+  XFile? image;
+  String address = "", date = "", time = "", dateTime = "", status = "Attend";
+  bool isLoading = false;
+  double lat = 0.0, long = 0.0;
+  int dateHours = 0, minute = 0;
+  final controller = TextEditingController();
+  final CollectionReference dataCollection = FirebaseFirestore.instance.collection('attendance');
+
+  @override
+  void initState(){
+    handleLocationPermission();
+    setDate();
+    setAttendStatus();
+
+    if (image != null) {
+      isLoading = true;
+      getGeolocationPosition();
+    }
+  }
+
+  @override // anotasi
   Widget build(BuildContext context) {
-    return const Placeholder();
+    Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.blueAccent,
+        leading: IconButton(
+          onPressed: () => Navigator.of(context).pop(), 
+          icon: const Icon(Icons.arrow_back_ios)
+        ),
+        title: const Text(
+          "Attendance Report",
+          style:  TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.white
+          ),
+        )),
+
+        body:  SingleChildScrollView(
+          child: Card(
+            color: Colors.white,
+            margin: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10)
+            ),
+            elevation: 5,
+            child:  Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 50,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10)
+                    ),
+                    color: Colors.blueAccent
+                  ),
+                  child: const Row(
+                    children: [
+                      SizedBox(width: 12,),
+                      Icon(Icons.face_retouching_natural_outlined),
+                      SizedBox(width: 12,),
+                      Text(
+                        "Please scan your face",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(10, 20, 0, 20),
+                  child: Text(
+                    "Capture Image",
+                    style:  TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const CameraScreen())
+                    );
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.fromLTRB(10, 0, 10, 20),
+                    width: size.width,
+                    height: 150,
+                    child: DottedBorder(
+                      radius: const Radius.circular(10),
+                      borderType: BorderType.RRect,
+                      color: Colors.blueAccent,
+                      strokeWidth: 1,
+                      dashPattern: const [5,5],
+                      child:  SizedBox.expand(
+                        // membuat widget yang skalanya mengikuti si boxfitnya
+                        child: FittedBox(
+                          child: image != null 
+                          ? Image.file(File(image!.path), fit: BoxFit.cover) 
+                          : Icon(Icons.camera_enhance_outlined)
+                        )
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    
   }
 }
