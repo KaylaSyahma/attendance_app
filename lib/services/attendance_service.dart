27bbac38da8264dda56f2f16f12c6e26a0d30adb
KaylaSyahma <ksyahma22@gmail.com>
2025-01-28 09:48:31 +0700

    config attendance service file

--- lib/services/attendance_service.dart
+++ lib/services/attendance_service.dart
@@ -1,8 +1,80 @@
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:flutter/material.dart';
+import 'package:attendance_app/ui/home_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 
-// final CollectionReference dataCollection = FirebaseFirestore.instance.collection('attendance');
+// an entry point for submitting the attendance report
+final CollectionReference dataCollection = FirebaseFirestore.instance.collection('attendance');
 
-// Future<void> submitReport(BuildContext context, String address, String name, String status, String timestamp) async{
-//   showLoaderDialog(context);
-// }
+Future<void> submitReport(BuildContext context, String address, String name, String status, String timestamp) async{
+  showLoaderDialog(context);
+  dataCollection.add(
+    {
+      'Address' : address,
+      'name' : name,
+      'status' : status,
+      'timestamp' : timestamp
+    }
+  ).then((result){
+    Navigator.of(context).pop();
+    try {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Row(
+          children: [
+            Icon(
+              Icons.check_circle_outline,
+              color: Colors.white,
+            ),
+            SizedBox(width: 10,),
+            Text("Attendance Submitted Successfully", style: TextStyle(color: Colors.white),)
+          ],
+        ),
+        backgroundColor: Colors.orangeAccent,
+        shape: StadiumBorder(),
+        behavior: SnackBarBehavior.floating,
+      ));
+      Navigator.pushReplacement(
+        context, 
+        MaterialPageRoute(builder: (context) => const HomeScreen())
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar( SnackBar(
+        content:  Row(
+          children: [
+            Icon(
+              Icons.info_outline,
+              color: Colors.white,
+            ),
+            SizedBox(width: 10,),
+            Expanded(
+              child: Text("Oops, $e", style: TextStyle(color: Colors.white),)
+            )
+          ],
+        )
+      ));
+    }
+  });
+}
+
+void showLoaderDialog(BuildContext context) {
+  AlertDialog alert = AlertDialog(
+    content: Row(
+      children: [
+        const CircularProgressIndicator(
+          // ini yang basic loading yang muter muter itu
+          valueColor: AlwaysStoppedAnimation<Color>(Colors.blueAccent),
+        ),
+        Container(
+          margin: const EdgeInsets.only(left: 20),
+          child: const Text("Checking the Data.."),
+        )
+      ],
+    ),
+  );
+  showDialog(
+    barrierDismissible: false,
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    }
+  );
+}
