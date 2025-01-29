4d98dace1fdf34b3f3e8f23fc8910ef0622dbb56
KaylaSyahma <ksyahma22@gmail.com>
2025-01-29 10:35:54 +0700

    make a capture photo component

--- lib/ui/attend/components/header.dart
+++ lib/ui/attend/components/header.dart
@@ -3,14 +3,14 @@ import 'package:flutter/material.dart';
 Container buildHeader() {
   return Container(
     height: 50,
-    decoration: BoxDecoration(
+    decoration: const BoxDecoration(
       borderRadius: BorderRadius.only(
         topLeft: Radius.circular(10),
         topRight: Radius.circular(10)
       ),
       color: Colors.blueAccent
     ),
-  child: Row(
+  child: const Row(
     children: [
       SizedBox(width: 12,),
       Icon(Icons.face_retouching_natural_outlined, color: Colors.white,),
