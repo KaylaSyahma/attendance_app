124bf3097554b8dbded3d67180b3a267eae1bc52
KaylaSyahma <ksyahma22@gmail.com>
2025-01-29 10:16:26 +0700

    make a header component

---
+++ lib/ui/attend/components/header.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+Container buildHeader() {
+  return Container(
+    height: 50,
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(10),
+        topRight: Radius.circular(10)
+      ),
+      color: Colors.blueAccent
+    ),
+  child: Row(
+    children: [
+      SizedBox(width: 12,),
+      Icon(Icons.face_retouching_natural_outlined, color: Colors.white,),
+      SizedBox(width: 12,),
+      Text("Please tak e a picture", style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),)
+    ],
+  ),
+  );
+}
